964fea49283d989b4845fe49f2359dff4e79eef6
saisree <saisree@INsaisree.local>
2015-09-04 17:13:57 +0530

    modified spec for two items which are exempted

--- src/BillGenerator.java
+++ src/BillGenerator.java
@@ -1,3 +1,4 @@
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -5,18 +6,21 @@ import java.util.List;
 //Bill generator is which generates the bill for the goods purchased by the customer
 public class BillGenerator {
     public String bill(String listOfItems) {
-        Double totalCost = null;
+        Double totalCost = 0.0;
         List<List<String>> listOfSplitedItems= new ArrayList<>();
         List<String> splitedListOfItems=getSplitList(listOfItems,"\n");
         for (int i = 0; i < splitedListOfItems.size(); i++) {
-            listOfSplitedItems.add(splitedListOfItems);
+            List<String> splitedItem=getSplitList(splitedListOfItems.get(i)," ");
+            listOfSplitedItems.add(splitedItem);
             int sizeOfSplitedItem1=listOfSplitedItems.get(i).size();
-             totalCost+= new Double( listOfSplitedItems.get(i).get(sizeOfSplitedItem1 - 1));
+            totalCost+= new Double( listOfSplitedItems.get(i).get(sizeOfSplitedItem1 - 1));
         }
+
+        DecimalFormat df = new DecimalFormat("#.##");
 
         return listOfItems + "\n" +
                 " SalesTax:0.00\n" +
-                " Total :" + totalCost;
+                " Total :" + df.format(totalCost);
     }
 
     private List<String> getSplitList(String item, String ch) {
